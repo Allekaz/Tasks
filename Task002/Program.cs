b571a1556976eb89ebf604aa472e44d66cfe65b0
Alexey Azizov <alekazi@yandex.ru>
2022-11-21 14:53:07 +0300

    modified: Program.cs and .gitignore

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Пример:
+a = 5; b = 7->max = 7
+a = 2 b = 10->max = 10
+a = -9 b = -3->max = -3
+ */
+
+Console.Clear();
+Console.WriteLine("Поиск максимального числа из двух");
+
+Console.WriteLine("Введите первое число: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+if (number_a > number_b) Console.WriteLine($"Максимальное число {number_a}");
+else Console.WriteLine($"Максимальное число {number_b}");
