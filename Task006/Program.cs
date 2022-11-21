6a743feee7e5f28f983d29b60d613d98167efacb
Alexey Azizov <alekazi@yandex.ru>
2022-11-21 17:41:52 +0300

    modified:   Task006/Program.cs

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -5,4 +5,13 @@
 -3 -> нет
 7 -> нет
 */
-Console.Clear()
+Console.Clear();
+Console.WriteLine("Является ли число четным");
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remainder = number % 2;
+
+if (remainder == 0) Console.WriteLine("Да, это число является четным");
+else Console.WriteLine("Нет, это число является нечетным");
