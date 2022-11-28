9936b6a18c99f645385ec1a1ee7712a653d01518
Alexey Azizov <alekazi@yandex.ru>
2022-11-28 11:10:54 +0300

    modified:   Task015/Program.cs

--- Task015/Program.cs
+++ Task015/Program.cs
@@ -1,13 +1,21 @@
-﻿Console.Clear();
-Console.Write("Введите число от 1 до 7: ");
+﻿// Задача 15:
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Является ли день выходным? ");
+Console.Write("Введите число обозначающее день недели от 1 до 7: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
 if (day >= 1 && day <= 5)
 {
-        Console.WriteLine("Рабочий день!");
-} 
+    Console.WriteLine("Нет!");
+}
 else if (day >= 6 && day <= 7)
 {
-    Console.WriteLine("Выходной день!");
+    Console.WriteLine("Да!");
 }
 else Console.Write(" Нет такого дня недели!");
