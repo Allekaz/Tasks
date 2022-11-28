2954e60b8b6d946d1c937e0ffe2bf01fcad0b7b7
Alexey Azizov <alekazi@yandex.ru>
2022-11-28 10:59:55 +0300

    new file:   Task015/Program.cs new file:   Task015/Task015.csproj

---
+++ Task015/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day >= 1 && day <= 5)
+{
+        Console.WriteLine("Рабочий день!");
+} 
+else if (day >= 6 && day <= 7)
+{
+    Console.WriteLine("Выходной день!");
+}
+else Console.Write(" Нет такого дня недели!");
